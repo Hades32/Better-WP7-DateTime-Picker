ab409342baac32d3468286a863d48e966afa4146
Martin Rauscher <hades32@gmail.com>
2011-10-04 14:26:07 +0200

    initial commit

---
+++ WP7DateTimePicker/DateTimePicker.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WP7DateTimePicker
+{
+    public partial class DateTimePicker : UserControl
+    {
+        #region DateTime
+
+        /// <summary>
+        /// DateTime Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty DateTimeProperty =
+            DependencyProperty.Register("DateTime", typeof(DateTime), typeof(DateTimePicker),
+                new PropertyMetadata((DateTime)DateTime.Now,
+                    new PropertyChangedCallback(OnDateTimeChanged)));
+
+        /// <summary>
+        /// Gets or sets the DateTime property. This dependency property 
+        /// indicates the selected date and or time.
+        /// </summary>
+        public DateTime DateTime
+        {
+            get { return (DateTime)GetValue(DateTimeProperty); }
+            set { SetValue(DateTimeProperty, value); }
+        }
+
+        /// <summary>
+        /// Handles changes to the DateTime property.
+        /// </summary>
+        private static void OnDateTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DateTimePicker target = (DateTimePicker)d;
+            DateTime oldValue = (DateTime)e.OldValue;
+            DateTime newValue = target.DateTime;
+            target.OnDateTimeChanged(oldValue, newValue);
+        }
+
+        /// <summary>
+        /// Provides derived classes an opportunity to handle changes to the DateTime property.
+        /// </summary>
+        protected virtual void OnDateTimeChanged(DateTime oldValue, DateTime newValue)
+        {
+            updateText(newValue);
+        }
+
+        #endregion
+
+        #region PickDate
+
+        /// <summary>
+        /// PickDate Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty PickDateProperty =
+            DependencyProperty.Register("PickDate", typeof(bool), typeof(DateTimePicker),
+                new PropertyMetadata((bool)true,
+                    new PropertyChangedCallback(OnPickDateChanged)));
+
+        /// <summary>
+        /// Gets or sets the PickDate property. This dependency property 
+        /// indicates if a date is to be picked.
+        /// </summary>
+        public bool PickDate
+        {
+            get { return (bool)GetValue(PickDateProperty); }
+            set { SetValue(PickDateProperty, value); }
+        }
+
+        /// <summary>
+        /// Handles changes to the PickDate property.
+        /// </summary>
+        private static void OnPickDateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DateTimePicker target = (DateTimePicker)d;
+            bool oldValue = (bool)e.OldValue;
+            bool newValue = target.PickDate;
+            target.OnPickDateChanged(oldValue, newValue);
+        }
+
+        /// <summary>
+        /// Provides derived classes an opportunity to handle changes to the PickDate property.
+        /// </summary>
+        protected virtual void OnPickDateChanged(bool oldValue, bool newValue)
+        {
+        }
+
+        #endregion
+
+        #region PickTime
+
+        /// <summary>
+        /// PickTime Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty PickTimeProperty =
+            DependencyProperty.Register("PickTime", typeof(bool), typeof(DateTimePicker),
+                new PropertyMetadata((bool)true,
+                    new PropertyChangedCallback(OnPickTimeChanged)));
+
+        /// <summary>
+        /// Gets or sets the PickTime property. This dependency property 
+        /// indicates if a time is to be picked.
+        /// </summary>
+        public bool PickTime
+        {
+            get { return (bool)GetValue(PickTimeProperty); }
+            set { SetValue(PickTimeProperty, value); }
+        }
+
+        /// <summary>
+        /// Handles changes to the PickTime property.
+        /// </summary>
+        private static void OnPickTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DateTimePicker target = (DateTimePicker)d;
+            bool oldValue = (bool)e.OldValue;
+            bool newValue = target.PickTime;
+            target.OnPickTimeChanged(oldValue, newValue);
+        }
+
+        /// <summary>
+        /// Provides derived classes an opportunity to handle changes to the PickTime property.
+        /// </summary>
+        protected virtual void OnPickTimeChanged(bool oldValue, bool newValue)
+        {
+        }
+
+        #endregion
+
+
+
+        public DateTimePicker()
+        {
+            InitializeComponent();
+            updateText(this.DateTime);
+            this.Loaded += new RoutedEventHandler(DateTimePicker_Loaded);
+        }
+
+        private void updateText(System.DateTime dt)
+        {            
+            if (this.PickDate && this.PickTime)
+            {
+                this.Text.Text = dt.ToString();
+            }
+            else if (this.PickDate)
+            {
+                this.Text.Text = dt.ToLongDateString();
+            }
+            else //time
+            {
+                this.Text.Text = dt.ToLongTimeString();
+            }
+
+        }
+
+        void DateTimePicker_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (PickerPage.DateTime.HasValue)
+            {
+                this.DateTime = PickerPage.DateTime.Value;
+                PickerPage.DateTime = null;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PickerPage.DateTime = this.DateTime;
+            PickerPage.DateVisible = this.PickDate;
+            PickerPage.TimeVisible = this.PickTime;
+            var frame = Application.Current.RootVisual as Microsoft.Phone.Controls.PhoneApplicationFrame;
+            if (frame != null)
+            {
+                frame.Navigate(new Uri("/WP7DateTimePicker;component/PickerPage.xaml", UriKind.Relative));
+            }
+            else
+            {
+                throw new InvalidOperationException("Applications RootVisual must be a PhoneApplicationFrame");
+            }
+        }
+    }
+}
---
+++ WP7DateTimePicker/PickerPage.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Text.RegularExpressions;
+
+namespace WP7DateTimePicker
+{
+    public partial class PickerPage : PhoneApplicationPage
+    {
+        public static DateTime? DateTime { get; set; }
+        public static bool TimeVisible { get; set; }
+        public static bool DateVisible { get; set; }
+
+        static PickerPage()
+        {
+            DateTime = null;
+            TimeVisible = true;
+            DateVisible = true;
+        }
+
+        public PickerPage()
+        {
+            InitializeComponent();
+
+            if (!TimeVisible && !DateVisible)
+                throw new InvalidOperationException("Either Time or Date bust be visible!");
+            else
+            {
+                this.timeStack.Visibility = TimeVisible ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+                this.dateStack.Visibility = DateVisible ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+            }
+
+            if (DateTime == null)
+                DateTime = System.DateTime.Now;
+
+            this.hourTB.Text = DateTime.Value.Hour.ToString();
+            this.minuteTB.Text = DateTime.Value.Minute.ToString();
+
+            this.dayTB.Text = DateTime.Value.Day.ToString();
+            this.monthTB.Text = DateTime.Value.Month.ToString();
+            this.yearTB.Text = DateTime.Value.Year.ToString();
+        }
+
+        private void OKappbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime = new DateTime(toInt(this.yearTB), toInt(this.monthTB), toInt(this.dayTB),
+                                        toInt(this.hourTB), toInt(this.minuteTB), 0);
+                this.NavigationService.GoBack();
+            }
+            catch
+            {
+                MessageBox.Show("Not a valid date or time");
+            }
+        }
+
+        private void CancelAppBtn_Click(object sender, EventArgs e)
+        {
+            DateTime = null;
+            try
+            {
+                this.NavigationService.GoBack();
+            }
+            catch
+            { /* navigation... */ }
+        }
+
+        private static int toInt(TextBox tb)
+        {
+            if (string.IsNullOrEmpty(tb.Text))
+                return 0;
+            else
+                return Convert.ToInt32(tb.Text);
+        }
+
+        private void TB_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var tb = (TextBox)sender;
+            tb.SelectAll();
+        }
+
+        private void hourTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            checkNumber(e, 0, 23, 2, this.minuteTB);
+        }
+
+        private void minuteTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            checkNumber(e, 0, 59, 2, this.dayTB);
+        }
+
+        private void dayTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            checkNumber(e, 1, 31, 2, this.monthTB);
+        }
+
+        private void monthTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            checkNumber(e, 1, 12, 2, this.yearTB);
+        }
+
+        private void yearTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            checkNumber(e, System.DateTime.MinValue.Year, System.DateTime.MaxValue.Year, 4, this.hourTB);
+        }
+
+        Regex numberKey = new Regex("^D[0-9]$", RegexOptions.Compiled);
+
+        private void checkNumber(KeyEventArgs e, int min, int max, int max_length, TextBox nextTB)
+        {
+            if (numberKey.IsMatch(e.Key.ToString()))
+            {
+                var newText = ((TextBox)e.OriginalSource).Text + e.Key.ToString()[1];
+                var sel = ((TextBox)e.OriginalSource).SelectedText;
+                if (string.IsNullOrEmpty(sel) == false)
+                {
+                    newText = newText.Replace(sel, "");
+                }
+                int x;
+                if (int.TryParse(newText, out x))
+                {
+                    if (x > max || newText.Length > max_length)
+                    {
+                        e.Handled = true;
+                        nextTB.Focus();
+                    }
+                }
+            }
+            else if (e.Key == Key.Back)
+            {
+                // always ok
+            }
+            else if (e.PlatformKeyCode == 190) //if (e.Key == Key.Decimal) doesn't work...
+            {
+                // neither date or time needs a decimal point (or comma) so
+                // we (mis-)use it as a "tab" key
+                e.Handled = true;
+                nextTB.Focus();
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
